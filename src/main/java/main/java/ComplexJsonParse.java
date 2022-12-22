242a3d10bdf27ffd81f676374185ecbeda73a09a
DopeAbhi <krivrm1520@gmail.com>
2022-12-22 10:43:30 +0530

    Third Commit

--- src/main/java/main/java/ComplexJsonParse.java
+++ src/main/java/main/java/ComplexJsonParse.java
@@ -4,15 +4,46 @@ import files.Payload;
 import io.restassured.path.json.JsonPath;
 
 public class ComplexJsonParse {
-    public static void main(String[]args)
-    {
-        JsonPath js =new JsonPath(Payload.CoursePrice());
+    public static void main(String[] args) {
+        JsonPath js = new JsonPath(Payload.CoursePrice());
         int count = js.getInt("courses.size ()");
         System.out.println(count);
-        int totalAmount= js.getInt("dashboard.purchaseAmount");
+        //Print Purchase Amount
+        int totalAmount = js.getInt("dashboard.purchaseAmount");
         System.out.println(totalAmount);
-        String title=js.getString("courses[1].title");
+        //Print Title of the Course
+        String title = js.getString("courses[1].title");
         System.out.println(title);
 
+        //Print all courses title and thier respective prices
+
+        for (int i = 0; i < count; i++) {
+            String courseTitle = js.get("courses[" + i + "].title");
+            System.out.println(js.get("courses[" + i + "].price ").toString());
+            System.out.println(courseTitle);
+        }
+
+        //Print no. of copies of specific course
+        for (int i = 0; i < count; i++) {
+            String courseTitle = js.get("courses[" + i + "].title");
+            if (courseTitle.equalsIgnoreCase("RPA")) {
+                int copies = js.get("courses[" + i + "].copies");
+                System.out.println(copies);
+                break;
+            }
+        }
+        // Sum of Price Equals to the Purchase Amount
+        int sum=0;
+        for (int i=0; i<count; i++)
+        {
+             sum= sum + js.getInt("courses["+i+"].price")*js.getInt("courses["+i+"].copies");
+        }
+        int dasboard=js.getInt("dashboard.purchaseAmount");
+
+        if (dasboard == sum)
+        {
+            System.out.println(sum);
+        }
     }
+
 }
---
+++ src/main/java/main/java/SumValidation.java
@@ -0,0 +1,26 @@
+package main.java;
+import files.Payload;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SumValidation {
+
+    @Test
+    public void sumOfCourses()
+    {
+        JsonPath js=new JsonPath(Payload.CoursePrice());
+        int count= js.getInt("courses.size()");
+        int sum=0;
+        for (int i=0; i<count; i++)
+        {
+            sum= sum + js.getInt("courses["+i+"].price")*js.getInt("courses["+i+"].copies");
+        }
+        int dasboard=js.getInt("dashboard.purchaseAmount");
+
+        Assert.assertEquals(sum,dasboard);
+        System.out.println("Total Price "+sum);
+
+    }
+
+}
